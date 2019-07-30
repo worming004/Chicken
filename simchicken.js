3f2fe8d6c6a88d1261186dd26666e875661f7e22
Mathieu Scolas <mathieu.scolas@gmail.com>
2019-07-30 08:20:33 +0200

    add workspace

---
+++ simchicken.js
@@ -0,0 +1,47 @@
+exports.simchicken = (input) => {
+    input
+        .split('\r\n')
+        .map(wordToChicken)
+        .join('\r\n');
+}
+
+const wordToChicken = (word, index) => {
+    switch (word) {
+        case 'axe':
+            return howMuchChicken(0);
+        case 'chicken':
+            return howMuchChicken(1);
+        case 'add':
+            return howMuchChicken(2);
+        case 'fox':
+            return howMuchChicken(3);
+        case 'rooster':
+            return howMuchChicken(4);
+        case 'compare':
+            return howMuchChicken(5);
+        case 'pick':
+            return howMuchChicken(6);
+        case 'peck':
+            return howMuchChicken(7);
+        case 'fr':
+            return howMuchChicken(8);
+        case 'BBQ':
+            return howMuchChicken(9);
+    }
+    if (word.startsWith('push')) {
+        const splitted = word.split(' ');
+        return howMuchChicken(splitted[1] - 10);
+    }
+    throw new Error(`impossible to parse ${word} at line ${index}`);
+}
+
+const howMuchChicken = (nbr) => {
+    result = "";
+    let first = true;
+
+    for (var i = 0; i < nbr; i++) {
+        if (!first) result += ' ';
+        first = true;
+        result += 'chicken';
+    }
+}
